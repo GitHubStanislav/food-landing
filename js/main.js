daa707b5beeff7aeea6453ef60ce012275ca96d5
Stas Spiridonov <limserv17@gmail.com>
2023-01-27 11:04:54 +0200

    "Add modal window"

--- js/main.js
+++ js/main.js
@@ -79,17 +79,37 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // modal window
 
-  const modalTrigger = document.querySelector("[data-modal]");
+  const modalTrigger = document.querySelectorAll("[data-modal]");
   const modal = document.querySelector(".modal");
   const modalCloseBtn = document.querySelector("[data-close]");
 
-  modalTrigger.addEventListener("click", () => {
+  function openModal() {
     modal.style.display = "block";
     document.body.style.overflow = "hidden";
+    clearInterval(modalTimerID);
+  }
+  modalTrigger.forEach((e) => {
+    e.addEventListener("click", openModal);
   });
 
-  modalCloseBtn.addEventListener("click", () => {
+  function closeModal() {
     modal.style.display = "none";
     document.body.style.overflow = "";
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
   });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      closeModal();
+    }
+  });
+
+  const modalTimerID = setTimeout(openModal, 3000);
 });
